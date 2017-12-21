ea902082a73bbc641998eb6b4c0403d5e8977216
Jeremy Jones <jer@jerjones.me>
2017-12-21 13:54:19 +0000

    typo

--- test_tournament.py
+++ test_tournament.py
@@ -7,7 +7,7 @@ class ParticipantTestCase(TestCase):
     def setUp(self):
         self.steve = Participant('Steve', 100)
 
-    def test_participanti_constructor(self):
+    def test_participant_constructor(self):
         self.assertEqual(self.steve.get_name(), 'Steve')
         self.assertEqual(self.steve.get_score(), 100)
         self.assertEqual(self.steve.get_prize(), None)
