f3b757edf7a1430c810018583fa8c8480f9db93c
Jeremy Jones <jer@jerjones.me>
2017-12-22 12:44:37 +0000

    parts 3 and 4

--- test_tournament.py
+++ test_tournament.py
@@ -56,7 +56,7 @@ class ParticipantSortTestCase(TestCase):
 
 class DistributePrizesTestCase(TestCase):
     def setUp(self):
-        self.skipTest("Parts III & IV")
+        # self.skipTest("Parts III & IV")
 
         self.prizes = {
             1: 50,
@@ -65,16 +65,111 @@ class DistributePrizesTestCase(TestCase):
         }
 
     def test_example_one(self):
-        pass
+        self.sally = Participant('Sally', 77)
+        self.betty = Participant('Betty', 66)
+        self.don = Participant('Don', 46)
+        self.megan = Participant('Megan', 45)
+
+        distribute_prizes(sort_participants(
+            [self.megan, self.don, self.betty, self.sally]),
+            self.prizes)
+
+        self.assertEqual(
+            [self.sally,     self.sally.get_prize(),
+             self.betty,     self.betty.get_prize(),
+             self.don,       self.don.get_prize(),
+             self.megan,     self.megan.get_prize()],
+
+            [self.sally,     50,
+             self.betty,     20,
+             self.don,       10,
+             self.megan,      0]
+        )
 
     def test_example_two(self):
-        pass
+        self.roger = Participant('Roger', 77)
+        self.jane = Participant('Jane', 66)
+        self.pete = Participant('Pete', 45)
+        self.cooper = Participant('Cooper', 45)
+
+        distribute_prizes(sort_participants(
+            [self.roger, self.jane, self.pete, self.cooper]),
+            self.prizes)
+
+        self.assertEqual(
+            [self.roger,      self.roger.get_prize(),
+             self.jane,       self.jane.get_prize(),
+             self.pete,       self.pete.get_prize(),
+             self.cooper,     self.cooper.get_prize()],
+
+            [self.roger,      50,
+             self.jane,       20,
+             self.pete,        5,
+             self.cooper,      5]
+        )
 
     def test_example_three(self):
-        pass
+        self.peggy = Participant('Peggy', 77)
+        self.tom = Participant('Tom', 66)
+        self.joan = Participant('Joan', 66)
+        self.ken = Participant('Ken', 45)
+
+        distribute_prizes(sort_participants(
+            [self.peggy, self.tom, self.joan, self.ken]),
+            self.prizes)
+
+        self.assertEqual(
+            [self.peggy,       self.peggy.get_prize(),
+             self.tom,         self.tom.get_prize(),
+             self.joan,        self.joan.get_prize(),
+             self.ken,         self.ken.get_prize()],
+
+            [self.peggy,       50,
+             self.tom,         15,
+             self.joan,        15,
+             self.ken,          0]
+        )
 
     def test_example_four(self):
-        pass
+        self.harry = Participant('Harry', 77)
+        self.lane = Participant('Lane', 45)
+        self.stan = Participant('Stan', 45)
+        self.henry = Participant('Henry', 45)
+
+        distribute_prizes(sort_participants(
+            [self.harry, self.lane, self.stan, self.henry]),
+            self.prizes)
+
+        self.assertEqual(
+            [self.harry,        self.harry.get_prize(),
+             self.lane,         self.lane.get_prize(),
+             self.stan,         self.stan.get_prize(),
+             self.henry,        self.henry.get_prize()],
+
+            [self.harry,        50,
+             self.lane,         10,
+             self.stan,         10,
+             self.henry,        10]
+        )
 
     def test_example_five(self):
-        pass
+        self.ted = Participant('Ted', 77)
+        self.trudy = Participant('Trudy', 77)
+        self.bobby = Participant('Bobby', 46)
+        self.paul = Participant('Paul', 46)
+
+        distribute_prizes(sort_participants(
+            [self.ted, self.trudy, self.bobby, self.paul]),
+            self.prizes)
+
+        self.assertEqual(
+            [self.ted,           self.ted.get_prize(),
+             self.trudy,         self.trudy.get_prize(),
+             self.bobby,         self.bobby.get_prize(),
+             self.paul,          self.paul.get_prize()],
+
+            [self.ted,           35,
+             self.trudy,         35,
+             self.bobby,          5,
+             self.paul,           5]
+        )
--- tournament.py
+++ tournament.py
@@ -22,7 +22,7 @@ class Participant(object):
 
     def set_prize(self, prize):
         """
-        Set a specific prize. Doesn't add prizes.
+        Set a specific prize, REPLACING any previous prize.
         :param prize: integer
         """
         self.prize = prize
@@ -34,8 +34,38 @@ def sort_participants(participants):
     :param participants: list
     :return: list
     """
-    return sorted(participants, key=lambda _: _.get_score() or 0, reverse=True)
+    return sorted(participants, key=lambda p: p.get_score() or 0, reverse=True)
 
 
 def distribute_prizes(participants, prizes):
-    pass
+    """
+    Updates list of winners with their prize values, in-place.
+    :param participants: sorted list of Participant objects
+    :param prizes: map of prize values
+    :return: None
+    """
+    for pidx in range(len(participants)):
+        if pidx + 1 in prizes:
+            prize = prizes[pidx + 1]
+            del(prizes[pidx + 1])
+
+            myscore = participants[pidx].get_score()
+
+            winners = [pidx] # there might be more than one. remember the idx
+            nextPerson = pidx + 1
+
+            for additionalidx in range(nextPerson, len(participants)):
+                if participants[additionalidx].get_score() == myscore:
+                    winners.append(additionalidx) # they also win
+
+                    if additionalidx + 1 in prizes: # and there's more money
+                        prize += prizes[additionalidx + 1]
+                        del(prizes[additionalidx + 1])
+                else:
+                    break
+
+            for winner in winners:
+                participants[winner].set_prize(prize / len(winners))
+
+        elif participants[pidx].get_prize() is None:
+            participants[pidx].set_prize(0)
